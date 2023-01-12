bcbd8ff29df525f87b4b4e782cc9cac05f3c8ac1
VisaR <69953716+Visha17@users.noreply.github.com>
2023-01-12 12:04:28 +0530

    Create sorted_list.py

---
+++ sorted_list.py
@@ -0,0 +1,9 @@
+list1 = [1, 5, 6, 9, 11]
+list2 = [3, 4, 7, 8, 10]
+ 
+print ("The original list 1 is : " + str(list1))
+print ("The original list 2 is : " + str(list2))
+ 
+result = sorted(list1 + list2)
+
+print ("The combined sorted list is : " + str(result))
